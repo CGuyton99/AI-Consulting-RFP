9ce8ffdd76fcbfd5158e2c035815e9f0aeb6fccf
Christian Guyton <cguyton@icstars.org>
2022-06-14 09:07:15 -0500

    Font installed + Home Page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,51 @@
 import React from "react";
-import './index.css'
-function Home(){
-    return(
-   <body className="homeback">
-       <main>
-        
-       </main>
-   </body>
-    );
-};
+import "./index.css";
+function Home() {
+  return (
+    <body className="homeback">
+      <main>
+        <div className="homesection">
+            <div className="center">
+                <h1>Quick Links/Map</h1>
+            </div>
+        </div>
+        <div className="homesection">
+          <div className="center">
+            <h1>Our Vision</h1>
+            <p>
+              In Milwaukee County, we are taking an all-hands approach to
+              realizing our vision that by achieving racial equity, Milwaukee is
+              the healthiest county in Wisconsin. Together we will dismantle
+              institutional racism in our organization and build up policies,
+              practices and a culture that ensures race no longer predicts
+              health outcomes, and outcomes for everyone improve.
+            </p>
+          </div>
+        </div>
+        <div className="homesection">
+          <div className="center">
+            <h1>News & Bulletin</h1>
+          </div>
+          <p>
+          <ul>
+            <li>test</li>
+            <li>test2</li>
+            <li>test3</li>
+            <li>Longer headline goes here</li>
+            <li>
+              Long headline that is very big and will wrap in the div goes here,
+              is this working? Please let this work
+            </li>
+            <li>Okay it worked, I'm relieved</li>
+            <li>Talking to myself</li>
+          </ul>
+          </p>
+          <div className="right">
+            <p>See More</p>
+          </div>
+        </div>
+      </main>
+    </body>
+  );
+}
 export default Home;
-
