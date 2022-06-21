bc085750d887d3eaedd6832ede4b89a7d7fbb083
Torrontayè Jones <tjones@icstars.org>
2022-06-21 13:25:39 -0500

    lol

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ import FNA from './pages/fna';
 import directories from './pages/directories';
 import Idle from './pages/Idle';
 import nab from './pages/newsAndbulletin';
-import { Outlet, Link,BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import navbar from './components/nav-bar/navbar';
 
 function App() {
   return (
-    <div>
+    <div id='App'>
+      <navbar />
+      <Router>
+        <link to= '/'>Home</link>
+        <link to= '/calendar'>Calendar</link>
+        <link to= '/careers'>Careers</link>
+        <link to= '/directories'>Directories</link>
+        <link to= '/fna'>Food and Amenities</link>
+          <Routes>
+              <Route path='/' element={<home/>}/>
+              <Route path='Calendar' element={<calendar/>}/>
+              <Route path='Careers' element={<careers/>}/>
+              <Route path='Directories' element={<directories/>}/>
+              <Route path= 'Food and Amenities' element={<fna/>}/>
+          </Routes>
+      </Router>
       <Home/>
     </div>  
   )
--- src/components/nav-bar/navbar.js
+++ src/components/nav-bar/navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./style.css";
 
-function navbar() {
+/*function navbar() {
   return (
 	<nav>
     <h3>
@@ -19,4 +19,36 @@ function navbar() {
   )
 }
 
-export default navbar
+export default navbar*/
+
+import { slide as Menu } from 'react-burger-menu';
+
+export default props => {
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+
+      <a className="menu-item" href="/calendar">
+        Calendar
+      </a>
+
+      <a className="menu-item" href="/careers">
+        Careers
+      </a>
+
+      <a className="menu-item" href="/directories">
+        Directory
+      </a>
+
+      <a className="menu-item" href="/fna">
+        Food & Amenities
+      </a>
+
+      <a className="menu-item" href="/newsAndbulletin">
+        News & Bulletin
+      </a>
+    </Menu>
+  );
+};
